12e6e3f48ce3212ab8b96605ff70909d3135fa09
torobucci <kevintoro254@gmail.com>
2023-05-10 18:24:51 +0300

    drop and create new animals  table with species_id and owner_id columns

--- schema.sql
+++ schema.sql
@@ -8,3 +8,34 @@ CREATE TABLE animals (
 );
 ALTER TABLE animals
 ADD COLUMN species varchar;
+
+CREATE TABLE owners (
+  id int GENERATED ALWAYS AS IDENTITY,
+  full_name varchar,
+  age int,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+  id int GENERATED ALWAYS AS IDENTITY,
+  name varchar,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE animals;
+CREATE TABLE animals (
+  id SERIAL PRIMARY KEY,
+  name varchar,
+  date_of_birth date,
+  escape_attempts int,
+  neutered bool,
+  weight_kg decimal,
+  species_id int,
+  owner_id int,
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+  CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id)
+);
+
+
+
+
