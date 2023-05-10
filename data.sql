291c46de96d019ceedc9c802760faabb9ad622f3
torobucci <kevintoro254@gmail.com>
2023-05-10 18:25:40 +0300

    insert data to animals table

--- data.sql
+++ data.sql
@@ -15,4 +15,41 @@ VALUES
   ('Blossom', '1998-10-13', 3, TRUE, 17),
   ('Ditto', '2022-05-14', 4, TRUE, 22);
 
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+    ('Agumon', '2020-02-03', 0, true, 10.23),
+    ('Gabumon', '2018-11-15', 2, true, 8),
+    ('Devimon', '2017-05-12', 5, true, 11),
+    ('Boarmon', '2005-06-07', 7, true, 20.4),
+    ('Pikachu', '2021-01-07', 1, false, 15.04),
+    ('Blossom', '1998-10-13', 3, true, 17),
+    ('Plantmon', '2021-11-15', 2, true, 5.7),
+    ('Angemon', '2005-06-12', 1, true, 45),
+    ('Charmander', '2020-02-08', 0, false, 11),
+    ('Squirtle', '1993-04-02', 3, false, 12.13);
 
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES
+ ('Pokemon'),
+ ('Digimon');
+
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name LIKE '%mon';
+
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE species_id IS NULL;
+
+UPDATE animals
+SET owner_id =
+  CASE
+    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+  END;
